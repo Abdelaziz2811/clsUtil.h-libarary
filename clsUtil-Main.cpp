f62aaaf80bd379dde1c56f4b1265970133640812
abdelaziz <abdobm2811@gmail.com>
2024-11-25 16:05:35 +0100

    Update clsUtil-Main.cpp

--- clsUtil-Main.cpp
+++ clsUtil-Main.cpp
@@ -1,104 +1,217 @@
+#pragma once
+
 #include <iostream>
 #include <string>
+#include <cstdlib>
 #include "clsDate.h"
-#include "clsPeriod.h"
-#include "clsUtil.h"
 
 using namespace std;
 
-int main() {
-	
-	clsUtil::Srand();
+class clsUtil {
 
-	cout << "Random Number : " << clsUtil::RandNumber(1, 9) << endl;
-	cout << "Random Character : " << clsUtil::GetRandChar(clsUtil::MixChars) << endl;
-	cout << "Generate Word : " << clsUtil::GenerateWord(clsUtil::CapitalLetter, 4) << endl;
-	cout << "Generate key : " << clsUtil::GenerateKey(clsUtil::CapitalLetter, 4) << endl;
-	cout << "Generate keys : \n";
-	clsUtil::GenerateKeys(clsUtil::CapitalLetter, 4, 4);
+public:
+	static void Srand() {
 
-	cout << "-----------------------------\n";
-
-	int x = 100, y = 200;
-	cout << "Numbers Before swap : " << x << " - " << y << endl;
-	clsUtil::SwapNumbers(x, y);
-	cout << "Numbers After swap : " << x << " - " << y << endl;
-
-	cout << "-----------------------------\n";
-
-	double w = 100.56, z = 200.23;
-	cout << "Numbers Before swap : " << w << " - " << z << endl;
-	clsUtil::SwapNumbers(w, z);
-	cout << "Numbers After swap : " << w << " - " << z << endl;
-
-	cout << "-----------------------------\n";
-
-	clsDate Date1(23, 11, 2024), Date2(12, 1, 2028);
-	cout << "Numbers Before swap : " << Date1.DateToString() << " - " << Date2.DateToString() << endl;
-	clsUtil::SwapNumbers(Date1, Date2);
-	cout << "Numbers After swap : " << Date1.DateToString() << " - " << Date2.DateToString() << endl;
-
-	cout << "-----------------------------\n";
-
-	int Arr1[5] = { 1,2,3,4,5 };
-	clsUtil::ShuffleArray(Arr1, 5);
-	cout << "Numbers after shuffling : ";
-	for (short i = 0; i < 5; i++) {
-
-		cout << Arr1[i] << " ";
+		srand((unsigned)time(NULL));
+	}
+	static int RandNumber(int From, int To)
+	{
+		int Number = rand() % (To - From + 1) + From;
+		return Number;
 	}
 
-	cout << "\n-----------------------------\n";
+	static enum enCharType { SmallLetter = 1, CapitalLetter = 2, Digit = 3, MixChars = 4, SpecialCharacter = 5 };
+	static char GetRandChar(enCharType CharType)
+	{
+		//to return MixCharacters; this way or the other one down there.
 
-	int Arr2[5];
-	clsUtil::FillArrayWithRandNumbers(Arr2, 5, 20, 99);
-	cout << "Array after fill with random numbers : ";
-	for (short i = 0; i < 5; i++) {
+		if (CharType == enCharType::MixChars)
+			CharType = (enCharType)RandNumber(1, 3);
 
-		cout << Arr2[i] << " ";
+		switch (CharType)
+		{
+		case enCharType::SmallLetter:
+			return char(RandNumber(97, 122));
+			break;
+		case enCharType::CapitalLetter:
+			return char(RandNumber(65, 90));
+			break;
+		case enCharType::SpecialCharacter:
+			return char(RandNumber(33, 47));
+			break;
+		case enCharType::Digit:
+			return char(RandNumber(48, 57));
+			break;
+			//way 2.
+		/*case enCharType::MixChars:
+			return GetRandChar((enCharType)RandNumber(1, 3));
+			break;*/
+		default:
+			return char(RandNumber(65, 90));
+			break;
+		}
 	}
 
-	cout << "\n-----------------------------\n";
-
-	string Arr3[5];
-	clsUtil::FillArrayWithRandWords(Arr3, 5, clsUtil::CapitalLetter, 4);
-	cout << "Array after fill with random Words : ";
-	for (short i = 0; i < 5; i++) {
-
-		cout << Arr3[i] << " ";
+	static string GenerateWord(enCharType CharType, short Length)
+	{
+		string Word = "";
+		for (int i = 1; i <= Length; i++)
+		{
+			Word = Word + GetRandChar(CharType);
+		}
+		return Word;
+	}
+	static string GenerateKey(enCharType CharType, short Length)
+	{
+		string Key = "";
+		Key += GenerateWord(CharType, Length) + "-";
+		Key += GenerateWord(CharType, Length) + "-";
+		Key += GenerateWord(CharType, Length) + "-";
+		Key += GenerateWord(CharType, Length);
+		return Key;
+	}
+	static void GenerateKeys(enCharType CharType, short Length, int NumberOfKeys)
+	{
+		for (int i = 1; i <= NumberOfKeys; i++)
+		{
+			cout << "Key [" << i << "] : " << GenerateKey(CharType, Length) << endl;
+		}
 	}
 
-	cout << "\n-----------------------------\n";
-
-	string Arr4[5];
-	clsUtil::FillArrayWithRandKeys(Arr4, 5, clsUtil::CapitalLetter, 4);
-	cout << "Array after fill with random Keys : \n";
-	for (short i = 0; i < 5; i++) {
-
-		cout << Arr4[i] << endl;
+	static void SwapNumbers(int& A, int& B)
+	{
+		int Temp;
+		Temp = A;
+		A = B;
+		B = Temp;
+	}
+	static void SwapNumbers(double& A, double& B)
+	{
+		double Temp;
+		Temp = A;
+		A = B;
+		B = Temp;
+	}
+	static void SwapNumbers(bool& A, bool& B)
+	{
+		bool Temp;
+		Temp = A;
+		A = B;
+		B = Temp;
+	}
+	static void SwapChars(char& A, char& B)
+	{
+		char Temp;
+		Temp = A;
+		A = B;
+		B = Temp;
+	}
+	static void SwapStrings(string& A, string& B)
+	{
+		string Temp;
+		Temp = A;
+		A = B;
+		B = Temp;
+	}
+	static void SwapDates(clsDate& A, clsDate& B)
+	{
+		clsDate Temp;
+		Temp = A;
+		A = B;
+		B = Temp;
 	}
 
-	cout << "-----------------------------\n";
+	static void ShuffleArray(int arr[100], short arrLength)
+	{
+		for (int i = 0; i < arrLength; i++)
+		{
+			SwapNumbers(arr[RandNumber(1, arrLength) - 1], arr[RandNumber(1, arrLength) - 1]);
+		}
+	}
 
-	cout << "Text 1 " << clsUtil::Tabs(2) << "Text 2" << endl;
+	static void FillArrayWithRandNumbers(int arr[100], short arrLength, short From, short To)
+	{
+		for (int i = 0; i < arrLength; i++)
+			arr[i] = RandNumber(From, To);
+	}
 
-	cout << "-----------------------------\n";
+	static void FillArrayWithRandWords(string arr[100], short arrLength, enCharType CharType, short WordLength)
+	{
+		for (int i = 0; i < arrLength; i++)
+			arr[i] = GenerateWord(CharType, WordLength);
+	}
 
-	const short EncryptionKey = 2;
+	static void FillArrayWithRandKeys(string arr[100], short arrLength, enCharType CharType, short WordLength)
+	{
+		for (int i = 0; i < arrLength; i++)
+			arr[i] = GenerateKey(CharType, WordLength);
+	}
 
-	string TextAfterEncryption, TextAfterDecryption;
-	string Text = "Hi, I am Abdelaziz Bahoumi, How are you doing";
-	cout << "Text : " << Text << endl;
-	
-	TextAfterEncryption = clsUtil::EncryptText(Text, EncryptionKey);
-	cout << "Text after encryption : " << TextAfterEncryption << endl;
+	static string Tabs(short NumberOfTabs) {
 
-	TextAfterDecryption = clsUtil::DecryptText(TextAfterEncryption, EncryptionKey);
-	cout << "Text after Decryption : " << TextAfterDecryption << endl;
+		string Tab;
 
-	cout << "-----------------------------\n";
+		for (short i = 1; i <= NumberOfTabs; i++)
+			Tab += "\t";
 
+		return Tab;
+	}
 
-	return 0;
-}
+	static string EncryptText(string Text, short EncryptionKey)
+	{
+		for (int i = 0; i <= Text.length(); i++)
+		{
+			Text[i] = char((int)Text[i] + EncryptionKey);
+		}
+		return Text;
+	}
+
+	static string DecryptText(string EncText, short EncryptionKey)
+	{
+		for (int i = 0; i <= EncText.length(); i++)
+		{
+			EncText[i] = char((int)EncText[i] - EncryptionKey);
+		}
+		return EncText;
+	}
+
+	static string NumberToText(int Number) {
+
+		if (Number == 0) {
+
+			return "Zero";
+		}
+		else if (Number >= 1 && Number <= 19) {
+
+			string UnitsUntil19[20] = { "","One","Two","Three" ,"Four" ,"Five" ,"Six" ,"Seven" ,"Eight"
+				,"Nine" ,"Ten","Eleven","Twelve","Thirteen","Fourteen" ,"Fifteen" ,"Sixteen"
+				,"Seventeen" ,"Eightteen" ,"Ninteen" };
+
+			return UnitsUntil19[Number];
+		}
+		else if (Number >= 20 && Number <= 99) {
+
+			string Tens[10] = { "","","Twenty","Thirty","Fourty","Fifty","Sixty","Seventy","Eighty","Ninty" };
+
+			return Tens[Number / 10] + " " + NumberToText(Number % 10);
+		}
+		else if (Number >= 100 && Number <= 999) {
+
+			return NumberToText(Number / 100) + " Hundred " + NumberToText(Number % 100);
+		}
+		else if (Number >= 1000 && Number <= 999999) {
+
+			return NumberToText(Number / 1000) + " Thousand " + NumberToText(Number % 1000);
+		}
+		else if (Number >= 1000000 && Number <= 999999999) {
+
+			return NumberToText(Number / 1000000) + " Million " + NumberToText(Number % 1000000);
+		}
+		else if (Number >= 1000000000 && Number <= 9999999999) {
+
+			return NumberToText(Number / 1000000000) + " Billion " + NumberToText(Number % 1000000000);
+		}
+	}
+
+};
 
